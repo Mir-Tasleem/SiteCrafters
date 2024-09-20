5860d6bbb101d6b8c190182b903f7811a3784eac
Mir-Tasleem <mirtasleem1234@gmail.com>
2024-09-20 16:38:13 +0530

    Project Added

---
+++ sitecrafters/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+    return(
+        <>
+            <div className="header">
+                <div className="company">
+                    <img src="" alt="" />
+                    <h1>SiteCrafters</h1>
+                </div>
+            </div>
+        </>
+    );
+}
